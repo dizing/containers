a5bdf72e5f0e3e8a23724bce8ffeaeb18d3f5b46
Julieta Helaine <julietah@ob-b3.nsk.21-school.ru>
2023-07-07 18:28:43 +0700

    Added iterator and push_back function

--- lib/list.h
+++ lib/list.h
@@ -17,14 +17,37 @@ struct ListNode : public BaseListNode {
   ListNode(Args &&...value) : value_(T(std::forward<Args>(value)...)) {}
 };
 
+template <typename T, bool isConst>
+struct ListIterator {
+  using value_type = T;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using base_node = BaseListNode;
+  using node = ListNode<value_type>;
+  base_node *node_;
+
+  ListIterator(base_node *node) : node_(node) {}
+
+  ListIterator<T, isConst> &operator++() {
+    node_ = node_->next;
+    return *this;
+  }
+  reference operator*() const { return static_cast<node *>(node_)->value_; }
+  base_node *GetNode() { return node_; }
+  bool operator==(const ListIterator &other) const {
+    return node_ == other.node_;
+  }
+  bool operator!=(const ListIterator &other) const { return !(*this == other); }
+};
+
 template <typename T, typename Allocator = std::allocator<T>>
 class list {
  public:
   using value_type = T;
   using reference = value_type &;
   using const_reference = const value_type &;
-  //   using iterator = value_type *;
-  //   using const_iterator = const value_type *;
+  using iterator = ListIterator<value_type, false>;
+  using const_iterator = ListIterator<value_type, true>;
   using size_type = std::size_t;
   using base_node = BaseListNode;
   using node = ListNode<value_type>;
@@ -53,13 +76,27 @@ class list {
   // Receives the element before which the new one will be created and parameter
   // pack for CreateNode
   template <typename... Args>
-  void InsertNodeBefore(base_node_pointer pos, Args &&...value) {
+  void InsertNodeBefore(iterator pos, Args &&...value) {
     node_pointer new_node = CreateNode(std::forward<Args>(value)...);
-    new_node->next = pos;
-    new_node->prev = pos->prev;
+    base_node_pointer node_from_pos = pos.GetNode();
+    new_node->next = node_from_pos;
+    new_node->prev = node_from_pos->prev;
     new_node->prev->next = new_node;
-    pos->prev = new_node;
+    node_from_pos->prev = new_node;
     ++size_;
+  }
+
+  iterator begin() { return fakeNode_.next; }
+
+  iterator end() { return &fakeNode_; }
+
+  void push_back(const_reference value_) {
+    InsertNodeBefore(end(), value_);
+    return;
+  }
+
+  void push_back(value_type &&value_) {
+    InsertNodeBefore(end(), std::move(value_));
   }
 
  private:
--- test/list_test.cc
+++ test/list_test.cc
@@ -2,7 +2,6 @@
 
 #include "containers.h"
 #include "gtest/gtest.h"
-
 class ListTest : public ::testing::Test {
  protected:
   ListTest() {}
@@ -12,34 +11,39 @@ class ListTest : public ::testing::Test {
 };
 
 struct testClass {
-    std::string a;
-    std::string b;
-    testClass() : a(""), b("") {
-        std::cout << "baseConstuctor" << std::endl;
-    }
+  std::string a;
+  std::string b;
+  testClass() : a(""), b("") { std::cout << "baseConstuctor" << std::endl; }
 
-    testClass(const std::string & a, const std::string & b) : a(a), b(b) {
-    
-        std::cout << "usualConstuctor" << std::endl;
-    }
+  testClass(const std::string& a, const std::string& b) : a(a), b(b) {
+    std::cout << "usualConstuctor" << std::endl;
+  }
 
-    testClass(const testClass& other) : a(other.a), b(other.b) {
-        std::cout << "copyConstuctor" << std::endl;
-    }
+  testClass(const testClass& other) : a(other.a), b(other.b) {
+    std::cout << "copyConstuctor" << std::endl;
+  }
 
-    testClass(testClass&& other) : a(std::move(other.a)), b(std::move(other.b)) {
-        std::cout << "moveConstuctor" << std::endl;
-    }
+  testClass(testClass&& other) : a(std::move(other.a)), b(std::move(other.b)) {
+    std::cout << "moveConstuctor" << std::endl;
+  }
 };
 
 TEST_F(ListTest, test) {
-  list<testClass> c;
-  testClass name = testClass("c", "d");
-  auto n1 = c.CreateNode(testClass("1", "2"));
-  c.InsertNode(n1, name);
-  c.InsertNode(static_cast<ListNode<testClass>*>(n1->next), "bbbb", "aaa");
-  for(int i = 0; i < 3; ++i) {
-    std::cout << n1->value_.a << std::endl;
-    n1 = static_cast<ListNode<testClass>*>(n1->next);
+  testClass name = testClass("Maron", "Kubanov");
+  std::cout << "----" << std::endl;
+  std::list<testClass> c;
+  c.push_back(testClass("1", "2"));
+  c.push_back(name);
+  // c.push_back("bbbb", "aaa");
+  for (auto& element : c) {
+    std::cout << element.a << std::endl;
+  }
+  std::cout << "----" << std::endl;
+  list<testClass> c1;
+  c1.push_back(testClass("1", "2"));
+  c1.push_back(name);
+  // c.push_back("bbbb", "aaa");
+  for (auto& element : c1) {
+    std::cout << element.a << std::endl;
   }
 }
