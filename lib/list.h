aa410e076084f0fdc5f3e33efbd600e0bce83931
Julieta Helaine <julietah@ob-b3.nsk.21-school.ru>
2023-07-07 21:21:20 +0700

    Refactored and added some wrapper functions

--- lib/list.h
+++ lib/list.h
@@ -32,6 +32,10 @@ struct ListIterator {
     node_ = node_->next;
     return *this;
   }
+  ListIterator<T, isConst> &operator--() {
+    node_ = node_->prev;
+    return *this;
+  }
   reference operator*() const { return static_cast<node *>(node_)->value_; }
   base_node *GetNode() { return node_; }
   bool operator==(const ListIterator &other) const {
@@ -54,12 +58,58 @@ class list {
   using node_alloc = typename Allocator::rebind<node>::other;
   using node_pointer = node *;
   using base_node_pointer = base_node *;
-  base_node fakeNode_ = {nullptr, nullptr};
+
+  // Constructors
 
   list() : alloc_(node_alloc()), size_(0) {
     fakeNode_.next = &fakeNode_;
     fakeNode_.prev = &fakeNode_;
   }
+
+  // Element Access
+  const_reference front() { return *begin(); }
+
+  const_reference back() { return *(--end()); }
+
+  // Iterators
+
+  iterator begin() { return fakeNode_.next; }
+
+  iterator end() { return &fakeNode_; }
+
+  // Capacity
+
+  bool empty() { return size() == 0; }
+
+  size_type size() { return size_; }
+
+  size_type max_size() { return size(); }
+
+  // Modifiers
+  iterator insert(iterator pos, const_reference value) {
+    InsertNodeBefore(pos, value);
+    return --pos;
+  };
+  iterator insert(iterator pos, value_type && value) {
+    InsertNodeBefore(pos, std::move(value));
+    return --pos;
+  }
+  void push_back(const_reference value) { InsertNodeBefore(end(), value); }
+
+  void push_back(value_type &&value) {
+    InsertNodeBefore(end(), std::move(value));
+  }
+
+  void push_front(const_reference value) { InsertNodeBefore(begin(), value); }
+
+  void push_front(value_type &&value) {
+    InsertNodeBefore(begin(), std::move(value));
+  }
+
+ private:
+  node_alloc alloc_;
+  size_type size_;
+  base_node fakeNode_ = {nullptr, nullptr};
 
   // Creates List Node with value_type element inside.
   // Receives universal reference parameter pack.
@@ -85,23 +135,6 @@ class list {
     node_from_pos->prev = new_node;
     ++size_;
   }
-
-  iterator begin() { return fakeNode_.next; }
-
-  iterator end() { return &fakeNode_; }
-
-  void push_back(const_reference value_) {
-    InsertNodeBefore(end(), value_);
-    return;
-  }
-
-  void push_back(value_type &&value_) {
-    InsertNodeBefore(end(), std::move(value_));
-  }
-
- private:
-  node_alloc alloc_;
-  size_type size_;
 };
 
 #endif  // CONTAINERS_LIB_LIST_H
--- test/list_test.cc
+++ test/list_test.cc
@@ -26,6 +26,9 @@ struct testClass {
   testClass(testClass&& other) : a(std::move(other.a)), b(std::move(other.b)) {
     std::cout << "moveConstuctor" << std::endl;
   }
+  bool operator==(const testClass& other) const {
+    return other.a == a && other.b == b;
+  }
 };
 
 TEST_F(ListTest, test) {
@@ -34,6 +37,8 @@ TEST_F(ListTest, test) {
   std::list<testClass> c;
   c.push_back(testClass("1", "2"));
   c.push_back(name);
+  c.push_front(testClass("me", "me"));
+  c.insert(++c.begin(), testClass("you", "you"));
   // c.push_back("bbbb", "aaa");
   for (auto& element : c) {
     std::cout << element.a << std::endl;
@@ -42,8 +47,12 @@ TEST_F(ListTest, test) {
   list<testClass> c1;
   c1.push_back(testClass("1", "2"));
   c1.push_back(name);
+  c1.push_front(testClass("me", "me"));
+  c1.insert(++c1.begin(), testClass("you", "you"));
   // c.push_back("bbbb", "aaa");
   for (auto& element : c1) {
     std::cout << element.a << std::endl;
   }
+  EXPECT_EQ(c1.front(), c.front());
+  EXPECT_EQ(c1.back(), c.back());
 }
